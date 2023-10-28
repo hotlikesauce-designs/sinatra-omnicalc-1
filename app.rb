47651d980e6659b35fe9726c464dcc63942ababd
hotlikesauce-designs <146861584+hotlikesauce-designs@users.noreply.github.com>
2023-10-28 16:07:51 +0000

    square root

--- app.rb
+++ app.rb
@@ -1,8 +1,8 @@
 require "sinatra"
 require "sinatra/reloader"
 
-get("/howdy") do
-  erb(:hello)
+get("/") do
+  erb(:homepage)
 end
 
 get("/square/new") do
@@ -10,9 +10,11 @@ get("/square/new") do
 end
 
 get("/square/results") do
+  @the_numb = params.fetch("users_number").to_f
+  @the_result = @the_numb**2 
   erb(:square_results)
 end
 
-get("/") do
-  erb(:homepage)
+get("/square_root/new") do
+  erb(:new_square_root_calc)
 end
