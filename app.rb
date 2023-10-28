31088b848d59d115a0fa7c9a714b8c27d17b19eb
hotlikesauce-designs <146861584+hotlikesauce-designs@users.noreply.github.com>
2023-10-28 16:11:53 +0000

    square root done

--- app.rb
+++ app.rb
@@ -18,3 +18,9 @@ end
 get("/square_root/new") do
   erb(:new_square_root_calc)
 end
+
+get("/square_root/results") do
+  @the_numb = params.fetch("users_number").to_f
+  @the_result = @the_numb**(0.5) 
+  erb(:square_root_results)
+end
